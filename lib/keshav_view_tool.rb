0f3999d36c9f4f65bbbb4fc71d38146cb4c32afe
Keshav <keshavbiswa21@gmail.com>
2018-04-26 19:25:49 +0530

    Dev tool initial commit

---
+++ lib/keshav_view_tool.rb
@@ -0,0 +1,6 @@
+require "keshav_view_tool/version"
+require "keshav_view_tool/renderer"
+
+module KeshavViewTool
+  # Your code goes here...
+end
---
+++ lib/keshav_view_tool/version.rb
@@ -0,0 +1,3 @@
+module KeshavViewTool
+  VERSION = "0.1.0"
+end
